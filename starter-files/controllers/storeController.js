0d6615e8170b8b3008bc3d09f9c5612f9893315e
Igor Tripolskiy <igor.tripolskiy@groupechantelle.com>
2018-03-26 17:19:35 +0300

    lesson 22

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -11,7 +11,7 @@ const multerOptions = {
     if (isPhoto) {
       next(null, true);
     } else {
-      next({ message: 'Filetype is invalid'}, false);
+      next({ message: 'Filetype is invalid' }, false);
     }
   }
 };
@@ -66,7 +66,26 @@ exports.updateStore = async (req, res) => {
     new: true, // return the new store instead of the old one
     runValidators: true
   }).exec();
-  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/store/${store.slug}">View Store →</a>`);
   res.redirect(`/stores/${store._id}/edit`);
-  // Redriect them the store and tell them it worked
+  // Redirect them the store and tell them it worked
+};
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) return next();
+  res.render('store', { store, title: store.name })
+};
+
+exports.getStoresByTag = async (req, res, next) => {
+  const tag = req.params.tag;
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tag });
+  const result = await Promise.all([
+    tagsPromise,
+    storesPromise
+  ]);
+  res.json(result);
+
+  // res.render('tags', { tags, title: 'Tags', tag });
 };
